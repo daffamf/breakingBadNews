d336b813251e30bab7f295c167b9157181c995f3
daffamf <daffamuhammadfirdaus5@gmail.com>
2021-11-26 19:18:04 +0700

    breakingbadnews

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import Header from './componenst/items/Header';
+import CharacterGrid from './componenst/characters/CharacterGrid';
+import Search from './componenst/items/Search';
+import { useEffect, useState } from 'react'
+import './App.css';
+import axios from 'axios'
+
+
+const App = () =>  {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState([])
+  const [query, setQuery] = useState('')
+
+
+  useEffect(() => {
+      const fetchItems = async () => {
+          const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+
+          console.log(result.data)
+          setItems(result.data)
+          setIsLoading(false)
+      }
+      fetchItems( )
+  }, [query ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+    <Header />
+    <Search getQuery={(q)=> setQuery(q)}/>
+    <CharacterGrid  isLoading={isLoading} items={items}/>
+    </div> 
   );
 }
 
---
+++ src/componenst/items/Header.js
@@ -0,0 +1,14 @@
+import logo from '../../img/logo.png'
+import React from 'react'
+
+
+const Header = () => {
+   
+    return (
+        <header className='center'>
+            <img src={logo} alt="" /> 
+        </header>
+    )
+}
+
+export default Header
